71e71ba5be9a91ab3dff6acdf2fbbbeb7620fea4
JoshuaHGeorge <jwatime@gmail.com>
2019-10-01 17:01:19 -0400

    Connected to sever and scafolded data context

---
+++ COMP2084GAssignment1/Models/Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace COMP2084GAssignment1.Models
+{
+    public partial class Class
+    {
+        public Class()
+        {
+            Event = new HashSet<Event>();
+        }
+
+        [Column("ClassID")]
+        public int ClassId { get; set; }
+        [Column("Class")]
+        [StringLength(50)]
+        public string Class1 { get; set; }
+
+        [InverseProperty("Class")]
+        public virtual ICollection<Event> Event { get; set; }
+    }
+}
---
+++ COMP2084GAssignment1/Models/Type.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace COMP2084GAssignment1.Models
+{
+    public partial class Type
+    {
+        public Type()
+        {
+            Event = new HashSet<Event>();
+        }
+
+        [Column("TypeID")]
+        public int TypeId { get; set; }
+        [Column("Type")]
+        [StringLength(50)]
+        public string Type1 { get; set; }
+
+        [InverseProperty("Type")]
+        public virtual ICollection<Event> Event { get; set; }
+    }
+}
